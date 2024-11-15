ff1ea7c60a9f32751e51efe45672f8945c2aed85
LucaBn <luca.bonotti@virgilio.it>
2024-11-15 20:56:33 +0100

    feat(HomePage): :sparkles: complete logic

--- src/components/UI/Templates/HomePage/HomePage.tsx
+++ src/components/UI/Templates/HomePage/HomePage.tsx
@@ -1,88 +1,75 @@
-import React, {
-  useState,
-  useEffect,
-  useMemo,
-  useCallback,
-  useRef,
-} from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 
 // Components
 import { Container, Row, Col, Button } from "react-bootstrap";
 import Keyboard from "@/components/UI/Organisms/Keyboard/Keyboard";
 
 // Constants
-import { wordList } from "@/constants/wordList";
+import { type Word, wordList } from "@/constants/wordList";
 
-// Utils
-import { shuffle } from "@/utils/array";
-import { getRandomIntInclusive } from "@/utils/number";
+const WORD_SEQUENCE_LENGTH = 20;
 
-const initialIndex: number = getRandomIntInclusive(0, wordList.length - 1);
+const createWordSequence = (): string[] => {
+  const usedWords = new Set<string>();
 
-// TODO: generate a chain of n words before starting the game, so I'll be sure there will not be errors
+  const getRandomUnusedWord = (): Word | undefined => {
+    const unusedWords = wordList.filter((w) => !usedWords.has(w.word));
+    if (unusedWords.length === 0) return undefined;
+    return unusedWords[Math.floor(Math.random() * unusedWords.length)];
+  };
+
+  const getNextUnusedWord = (currentWord: Word): Word | undefined => {
+    const possibleNextWords = currentWord.nextWordList
+      .map((nextWord) =>
+        wordList.find((w) => w.word === nextWord && !usedWords.has(w.word))
+      )
+      .filter((w): w is Word => w !== undefined);
+    if (possibleNextWords.length === 0) return undefined;
+    return possibleNextWords[
+      Math.floor(Math.random() * possibleNextWords.length)
+    ];
+  };
+
+  const sequence: string[] = [];
+
+  let currentWord = getRandomUnusedWord();
+
+  while (currentWord && sequence.length < WORD_SEQUENCE_LENGTH) {
+    sequence.push(currentWord.word);
+    usedWords.add(currentWord.word);
+
+    currentWord = getNextUnusedWord(currentWord);
+  }
+
+  return sequence;
+};
+
+let wordSequence: string[] = [];
+
+while (wordSequence.length < WORD_SEQUENCE_LENGTH) {
+  wordSequence = createWordSequence();
+}
+
+console.log({ wordSequence });
 
 const HomePage: React.FC = () => {
-  const [currentWordIndex, setCurrentWordIndex] =
-    useState<number>(initialIndex);
-  const [guessedWord, setGuessedWord] = useState<string>("");
-  const [previousGuessedWords, setPreviousGuessedWords] = useState<string[]>(
-    []
-  );
+  const [currentWordIndex, setCurrentWordIndex] = useState<number>(1);
+  const [guessedWord, setGuessedWord] = useState<string>(wordSequence[1][0]);
   const [buttonVariants, setButtonVariants] = useState<string[]>([]);
-  const [message, setMessage] = useState<string>("");
+  const [message, setMessage] = useState<string>("🤔");
+  const [slotHeight, setSlotHeight] = useState(56);
 
   const isKeyPressEnabled = useRef<boolean>(true);
-  const guessedWordRef = useRef<HTMLDivElement>(null);
-
-  const { word, nextWordList } = wordList[currentWordIndex];
-
-  const nextWord = useMemo(() => {
-    const filteredNextWords = nextWordList.filter(
-      (word) => !previousGuessedWords.includes(word)
-    );
-    if (filteredNextWords.length === 0) {
-      setMessage("Fine!");
-      return "";
-    }
-
-    const shuffedFilteredNextWords = shuffle(filteredNextWords);
-
-    if (previousGuessedWords.length === 0) {
-      setPreviousGuessedWords([word]);
-    }
-
-    const nextWordFirstLetter = shuffedFilteredNextWords[0][0];
-    setGuessedWord(nextWordFirstLetter);
-    return shuffedFilteredNextWords[0];
-  }, [currentWordIndex]);
 
   const updateButtonVariants = useCallback(() => {
     setButtonVariants(
-      nextWord
+      wordSequence[currentWordIndex]
         .split("")
         .map((_, index) =>
           guessedWord[index] ? "primary" : "outline-secondary"
         )
     );
-  }, [guessedWord, nextWord]);
-
-  const goToNextWord = (nextWord: string) => {
-    const newIndex = wordList.findIndex(
-      (wordObject) => wordObject.word === nextWord
-    );
-    if (newIndex !== -1) {
-      setCurrentWordIndex(newIndex);
-      if (guessedWordRef.current) {
-        guessedWordRef.current.scrollIntoView({
-          behavior: "smooth",
-          block: "center",
-        });
-      }
-    } else {
-      setGuessedWord("");
-      setMessage("Fine!");
-    }
-  };
+  }, [guessedWord]);
 
   const handleKeyPress = useCallback(
     (key: string) => {
@@ -90,30 +77,40 @@ const HomePage: React.FC = () => {
         return;
       }
 
+      setMessage("🤔");
+
       if (key === "DEL") {
         if (guessedWord.length >= 2) {
           setGuessedWord((prev) => prev.slice(0, -1));
         }
       } else if (key === "INVIO") {
-        if (guessedWord === nextWord) {
+        if (guessedWord === wordSequence[currentWordIndex]) {
           isKeyPressEnabled.current = false;
-          setMessage("Corretto!");
+          setMessage("😃");
           setTimeout(() => {
-            isKeyPressEnabled.current = true;
-            setPreviousGuessedWords((prev) => [...prev, guessedWord]);
-            goToNextWord(guessedWord);
+            if (currentWordIndex === WORD_SEQUENCE_LENGTH - 1) {
+              setMessage("Fine!");
+            } else {
+              setMessage("🤔");
+              setGuessedWord(wordSequence[currentWordIndex + 1][0]);
+              setCurrentWordIndex((prevValue) => prevValue + 1);
+              isKeyPressEnabled.current = true;
+            }
           }, 1000);
         } else {
-          setMessage("Prova un'altra parola!");
+          setMessage("😓");
         }
       } else if (
         /^[A-Za-z]$/.test(key) &&
-        guessedWord.length < nextWord.length
+        guessedWord.length < wordSequence[currentWordIndex].length
       ) {
+        console.log(wordSequence[currentWordIndex]);
+        console.log({ key });
+
         setGuessedWord((prev) => prev + key.toUpperCase());
       }
     },
-    [guessedWord, nextWord]
+    [guessedWord, currentWordIndex]
   );
 
   const handleKeydown = useCallback(
@@ -126,16 +123,27 @@ const HomePage: React.FC = () => {
   );
 
   useEffect(() => {
-    if (nextWord !== "") {
-      setMessage("");
-    }
     updateButtonVariants();
     window.addEventListener("keydown", handleKeydown);
     return () => window.removeEventListener("keydown", handleKeydown);
-  }, [guessedWord, word]);
+  }, [guessedWord]);
 
-  console.log({ isKeyPressEnabled });
-  console.log({ nextWord });
+  useEffect(() => {
+    const updateSlotHeight = () => {
+      if (window.innerWidth >= 768) {
+        setSlotHeight(64);
+      } else {
+        setSlotHeight(56);
+      }
+    };
+
+    updateSlotHeight();
+    window.addEventListener("resize", updateSlotHeight);
+
+    return () => {
+      window.removeEventListener("resize", updateSlotHeight);
+    };
+  }, []);
 
   return (
     <Container className="mt-5">
@@ -149,32 +157,45 @@ const HomePage: React.FC = () => {
         </Col>
       </Row>
 
-      <Row className="flex-column align-items-center mb-1 gap-1 gap-md-2">
-        {previousGuessedWords.map((prevWord, index) => (
-          <Col xs="auto" key={prevWord + index} className="mb-1 p-0">
-            <Button variant="warning" className="pointer-events-none">
-              {prevWord}
-            </Button>
-          </Col>
-        ))}
+      <Row>
+        <Col className="guessed-word__container overflow-hidden">
+          {wordSequence.map((word, wordSequenceIndex) => (
+            <Row
+              key={word}
+              className="guessed-word flex-nowrap justify-content-center gap-1 gap-md-2 pointer-events-none"
+              style={{
+                transform: `translateY(-${
+                  (currentWordIndex - 1) * slotHeight
+                }px)`,
+              }}
+            >
+              {word.split("").map((_, index) => (
+                <Col xs="auto" key={index} className="guessed-word__slot p-0">
+                  {wordSequenceIndex < currentWordIndex ? (
+                    <Button variant="primary" className="keyboard__btn p-1">
+                      {wordSequence[wordSequenceIndex][index]}
+                    </Button>
+                  ) : wordSequenceIndex === currentWordIndex ? (
+                    <Button
+                      variant={buttonVariants[index] || "outline-secondary"}
+                      className="keyboard__btn p-1"
+                    >
+                      {guessedWord[index]}
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="outline-secondary"
+                      className="keyboard__btn p-1"
+                    ></Button>
+                  )}
+                </Col>
+              ))}
+            </Row>
+          ))}
+        </Col>
       </Row>
 
-      <Row
-        ref={guessedWordRef}
-        className="guessedword justify-content-center mt-3 mb-1 gap-1 gap-md-2 pointer-events-none"
-      >
-        {nextWord.split("").map((_, index) => (
-          <Col xs="auto" key={index} className="guessedword__slot mb-1 p-0">
-            <Button
-              variant={buttonVariants[index] || "outline-secondary"}
-              className="keyboard__btn p-1"
-            >
-              {guessedWord[index]}
-            </Button>
-          </Col>
-        ))}
-        <p className="text-center">{message}</p>
-      </Row>
+      <p className="text-center mt-3">{message}</p>
 
       <Keyboard onKeyPress={handleKeyPress} />
     </Container>
