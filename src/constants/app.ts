3eaffe0eac99e33ae0a3699a862603505cd42ad4
LucaBn <luca.bonotti@virgilio.it>
2024-12-23 01:34:22 +0100

    feat(WordList): :sparkles: add lots of words

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -2,5 +2,6 @@ const APP_NAME: string = import.meta.env.VITE_APP_NAME;
 const APP_NAME_SHORT: string = import.meta.env.VITE_APP_NAME_SHORT;
 const AUTHOR_NAME: string = import.meta.env.VITE_AUTHOR_NAME;
 const WEBSITE_URL: string = import.meta.env.VITE_WEBSITE_URL;
+const RUN_TEST: string = import.meta.env.VITE_RUN_TEST;
 
-export { APP_NAME, APP_NAME_SHORT, AUTHOR_NAME, WEBSITE_URL };
+export { APP_NAME, APP_NAME_SHORT, AUTHOR_NAME, WEBSITE_URL, RUN_TEST };
