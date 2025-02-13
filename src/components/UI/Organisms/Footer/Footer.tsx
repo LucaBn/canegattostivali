f65702e18f86cdf24f6900f2afd540cd8c090125
LucaBn <luca.bonotti@virgilio.it>
2025-02-14 00:20:51 +0100

    feat(Footer): :speech_balloon: update texts

--- src/components/UI/Organisms/Footer/Footer.tsx
+++ src/components/UI/Organisms/Footer/Footer.tsx
@@ -16,9 +16,9 @@ const Footer: React.FC = () => {
       <Container>
         <Row>
           <Col xs={12} className="my-4">
-            <p className="text-center mb-1">© 2&zwj;024-2381 {siteUrl}</p>
+            <p className="text-center mb-1">© 2&zwj;025-2381 {siteUrl}</p>
             <p className="text-center mb-1">
-              Made with ♥ by{" "}
+              Creato con ♥ da{" "}
               <a
                 title="Visita la Pagina GitHub di LucaBn"
                 rel="noopener noreferrer nofollow"
