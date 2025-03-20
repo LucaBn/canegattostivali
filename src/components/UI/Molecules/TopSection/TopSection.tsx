a0323aec7130cc55ce7b27afc5231cfc8ab12755
LucaBn <luca.bonotti@virgilio.it>
2025-03-20 21:34:10 +0100

    refactor(TopSection): :recycle: update help text for clarity and improve layout

--- src/components/UI/Molecules/TopSection/TopSection.tsx
+++ src/components/UI/Molecules/TopSection/TopSection.tsx
@@ -5,29 +5,22 @@ import IconLightbulb from "@/components/UI/Atoms/IconLightbulb/IconLightbulb";
 const TopSection: React.FC = () => {
   return (
     <Row className="justify-content-center">
-      <Col xs="auto" md="8" lg="6" className="text-center">
+      <Col xs="auto" md="8" lg="6" className="text-center mb-5">
         <h1 className="mb-3">Cane Gatto Stivali</h1>
         <p className="mb-1">
           Indovina la parola usando quella precedente come indizio.
         </p>
-        <p className="mb-5">
+        <p className="mb-0">
           I tasti gialli forniscono un aiuto ☝🤓
           <br />
-          L'occhio{" "}
-          <span className="">
-            <IconEyeSlash />
-          </span>{" "}
-          filtra le lettere a tua disposizione.
+          L'occhio <IconEyeSlash /> filtra le lettere a tua disposizione.
           <br />
-          La lampadina{" "}
-          <span className="">
-            <IconLightbulb />
-          </span>{" "}
-          aggiunge una lettera alla parola da indovinare.
-          <span className="d-none d-lg-block mt-2">
-            Da desktop puoi usare i tasti <em>1</em> e <em>2</em> per usare gli
-            aiuti rapidamente!
-          </span>
+          La lampadina <IconLightbulb /> rivela una lettera in più della parola
+          da indovinare.
+        </p>
+        <p className="d-none d-lg-block mt-2 mb-0">
+          Da desktop puoi usare i tasti <em>1</em> e <em>2</em> per usare gli
+          aiuti rapidamente!
         </p>
       </Col>
     </Row>
