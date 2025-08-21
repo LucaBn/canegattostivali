44f37cecdeaa92c27ea2c827daf7a54af645d8bb
LucaBn <luca.bonotti@virgilio.it>
2025-08-21 11:59:38 +0200

    feat(PWA): :sparkles: add PWA support with vite-plugin-pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 
 // Providers
 import { Providers } from "@/components/providers";
 
 // CSS
 import "@/styles/index.scss";
+
+// Service Worker Registration
+registerSW();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      // devOptions: {
+      //   enabled: true,
+      // },
+    }),
+  ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
